3fdf7253170c6b8e81d10febb4af81245081d1de
smitrano <sarah.mitrano@gmail.com>
2016-03-24 16:06:20 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+   var winHeight = $(window).height();
+    $('.landing').css({
+        height: winHeight,
+        width: '100%'
+    });
+
+
+    $('.slide1').css({
+        height: winHeight,
+        width: '100%'
+ 	});
+
+ 	   $('.slide2').css({
+        height: winHeight,
+        width: '100%'
+ 	});
+
+ 	    $('.slide3').css({
+        height: winHeight,
+        width: '100%'
+ 	});
+
+ 	    $('.slide4').css({
+        height: winHeight,
+        width: '100%'
+ 	});
+
+ 	    $('.slide5').css({
+        height: winHeight,
+        width: '100%'
+ 	});
